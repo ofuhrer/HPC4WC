48804e7a0fc50bdb741988950ef7deed86a15c45
Crs01 Course <course01@daint101.login.cscs.ch>
2020-05-29 08:47:08 +0200

    adding stencil program

---
+++ day1/m_utils.F90
@@ -0,0 +1,252 @@
+! ******************************************************
+!      Module: m_utils
+!      Author: Oliver Fuhrer
+!       Email: oliver.fuhrer@vulcan.com
+!        Date: 09.09.2010
+! Description: Collection of utility routines for
+!              program flow managment and timing.
+! ******************************************************
+
+module m_utils
+    use mpi, only : MPI_COMM_WORLD, MPI_Abort
+    implicit none
+
+    integer, parameter :: stderr = 0
+
+    integer, parameter :: dp = 8
+    integer, parameter :: max_timing = 100
+
+    logical  :: ltiming = .false.
+    logical  :: ltiming_list(max_timing)
+    
+    character (len=28) :: tag_list(max_timing)
+    
+    integer :: cur_timing = 0
+    integer :: icountrate, icountmax
+    integer :: icountnew(max_timing)
+    integer :: icountold(max_timing)
+    integer :: ncalls(max_timing)
+    
+    real (kind=dp) :: stiming(max_timing)
+    real (kind=dp) :: rtiming(max_timing)
+    real (kind=dp) :: rsync(max_timing)
+    
+contains
+
+
+    function is_master()
+        implicit none
+
+        ! function value
+        logical :: is_master
+
+        is_master = my_rank() == 0
+
+    end function is_master
+
+
+    subroutine sync()
+        use mpi, only : MPI_COMM_WORLD, MPI_BARRIER
+        implicit none
+
+        ! local
+        integer :: ierror
+
+        call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+        call error(ierror /= 0, 'Problem with MPI_BARRIER', code=ierror)
+
+    end subroutine
+
+
+    function num_rank()
+        use mpi, only : MPI_COMM_WORLD, MPI_COMM_SIZE
+        implicit none
+
+        ! function value
+        integer :: num_rank
+
+        ! local
+        integer :: ierror
+
+        call MPI_COMM_SIZE(MPI_COMM_WORLD, num_rank, ierror)
+        call error(ierror /= 0, 'Problem with MPI_COMM_SIZE', code=ierror)
+
+    end function num_rank
+
+
+    function my_rank()
+        use mpi, only : MPI_COMM_WORLD, MPI_COMM_RANK
+        implicit none
+
+        ! function value
+        integer :: my_rank
+
+        ! local
+        integer :: ierror
+
+        call MPI_COMM_RANK(MPI_COMM_WORLD, my_rank, ierror)
+        call error(ierror /= 0, 'Problem with MPI_COMM_RANK', code=ierror)
+
+    end function my_rank
+
+
+    subroutine error(yes, msg, code)
+        implicit none
+
+        ! in
+        logical, intent(in) :: yes
+        character(len=*), intent(in) :: msg
+        integer, intent(in), optional :: code
+
+        ! local
+        integer :: ierror
+
+        if (yes) then
+            write(stderr,*) 'FATAL PROGRAM ERROR!'
+            write(stderr,*) msg
+            if (present(code)) then
+                write(stderr,*) code
+            end if
+            write(stderr,*) 'Execution aborted...'
+            call MPI_Abort(MPI_COMM_WORLD, 42, ierror)
+            stop
+        end if
+
+    end subroutine error
+
+
+    subroutine timer_init()
+        implicit none
+
+        ! local
+        integer :: idummy
+
+        ltiming         = .true.
+        ltiming_list(:) = .false.
+        rtiming(:)      = 0.0_dp
+        rsync(:)        = 0.0_dp
+        ncalls(:)       = 0
+        icountold(:)    = -1  ! set to -1 to signal that this timer has never been used
+        stiming(:) = -1.0_dp
+        icountrate  = 0
+        icountmax   = 0
+
+    end subroutine timer_init
+
+
+    subroutine timer_reset()
+        implicit none
+
+        rtiming(:) = 0.0_dp
+        rsync(:)   = 0.0_dp
+        ncalls(:)  = 0
+        stiming(:) = -1.0_dp
+
+    end subroutine timer_reset
+
+
+    subroutine timer_start(tag, inum)
+        use mpi, only : MPI_WTIME
+        implicit none
+
+        ! arguments
+        character (LEN=*), intent(in) :: tag
+        integer, intent(inout) :: inum
+
+        ! local
+        integer :: ierror
+
+        ! assign new index
+        if (inum <= 0) then
+            cur_timing = cur_timing + 1
+            inum = cur_timing
+        end if
+
+        ! check inum
+        call error(inum < 1 .or. inum > max_timing, &
+            'ERROR: Problem in start_loc_timing (inum < 1 or inum > max_timing)')
+
+        ! make sure this is the first call or previously end_loc_timing has been called
+        call error(icountold(inum) /= -1 .and. icountold(inum) /= -2, &
+            'ERROR: Problem in start_loc_timing (no previous end_loc_timing)')
+
+        ! save tag if this is the first call (check tag in debug mode otherwise)
+        if (icountold(inum) == -1) then
+            tag_list(inum) = trim(tag)
+            ltiming_list(inum) = .true.
+        end if
+
+        stiming(inum) = MPI_WTIME()
+        icountold(inum) = 1
+        ncalls(inum) = ncalls(inum) + 1
+
+    end subroutine timer_start
+
+
+    subroutine timer_end(inum)
+        use mpi, only : MPI_WTIME
+        implicit none
+
+        ! arguments
+        integer, intent(in) :: inum
+
+        ! local
+        real (kind=dp) :: etiming,ztime
+        integer :: ierror
+
+        call error(icountold(inum) == -1 .or. icountold(inum) == -2, &
+            'ERROR: Problem in end_loc_timing (no matching start_loc_timing)' )
+
+        etiming = MPI_WTIME()
+        ztime = etiming - stiming(inum)
+        icountnew(inum) = 1
+        rtiming(inum) = rtiming(inum) + ztime
+
+        ! release timer
+        icountold(inum) = -2 ! set to -2 to ensure a start_timing is called before next end_timing
+
+    end subroutine timer_end
+
+
+    subroutine timer_print()
+        use mpi, only : MPI_COMM_WORLD, MPI_DOUBLE_PRECISION, MPI_MIN, MPI_MAX, MPI_SUM
+        implicit none
+
+        ! local
+        integer :: inum
+        real (kind=dp) :: ztime_mean, ztime_min, ztime_max
+        integer :: ierror
+
+        if (ltiming) then
+
+            if (is_master()) print *, '--------------------------------------------------------------------------'
+            if (is_master()) print *, ' Timers:'
+            if (is_master()) WRITE(*, "(A,I4)") '   number of workers = ', num_rank()
+            if (is_master()) print *, '--------------------------------------------------------------------------'
+            if (is_master()) print *, ' Id      Tag                     #calls        min[s]        max[s]       mean[s] '
+
+            do inum = 1, max_timing
+
+                ! NOTE: this if-statement can go terribly wrong if not all locations are
+                ! called on all ranks (cf. MPI_REDUCE below)
+                if (ltiming_list(inum)) then 
+
+                    ! get run time
+                    call MPI_REDUCE(rtiming(inum), ztime_min, 1, MPI_DOUBLE_PRECISION, MPI_MIN, 0, MPI_COMM_WORLD, ierror)
+                    call MPI_REDUCE(rtiming(inum), ztime_max, 1, MPI_DOUBLE_PRECISION, MPI_MAX, 0, MPI_COMM_WORLD, ierror)
+                    call MPI_REDUCE(rtiming(inum), ztime_mean, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+                    ztime_mean = ztime_mean / REAL(num_rank(), dp)
+
+                    if (is_master()) WRITE(*,"(I4,A28,I8,F14.4,F14.4,F14.4)")  &
+                            inum, "      " // adjustl(tag_list(inum)), ncalls(inum), ztime_min, ztime_max, ztime_mean
+
+                end if
+            end do
+            if (is_master()) print *, '--------------------------------------------------------------------------'
+
+        end if
+
+    end subroutine timer_print
+
+
+end module m_utils
---
+++ day1/stencil_2d.F90
@@ -0,0 +1,270 @@
+! ******************************************************
+!     Program: stencil_2d
+!      Author: Oliver Fuhrer
+!       Email: oliverf@vulcan.com
+!        Date: 20.05.2020
+! Description: Simple stencil example
+! ******************************************************
+
+
+program main
+    use m_utils, only: timer_start, timer_end
+    implicit none
+
+    ! constants
+    integer, parameter :: wp = 4
+    
+    ! local
+    integer :: nx, ny, nz, num_iter
+    integer :: num_halo = 2
+    integer :: timer_work
+    real (kind=wp), allocatable :: in_field(:, :, :)
+    real (kind=wp), allocatable :: out_field(:, :, :)
+    integer :: istat
+    integer*8 :: flop_counter = 0, byte_counter = 0
+
+#ifdef CRAYPAT
+    include "pat_apif.h"
+    !call PAT_record( PAT_STATE_OFF, istat )
+#endif
+
+    call init()
+
+    ! warmup caches
+    call work(num_iter=1, increase_counters=.false.)
+
+#ifdef CRAYPAT
+    !call PAT_record( PAT_STATE_ON, istat )
+    call PAT_region_begin(1, 'work', istat )
+#endif
+    call timer_start('work', timer_work)
+
+    call work(num_iter=num_iter, increase_counters=.true.)
+    
+    call timer_end(timer_work)
+#ifdef CRAYPAT
+    call PAT_region_end(1, istat)
+    !call PAT_record( PAT_STATE_OFF, istat )
+#endif
+
+    call cleanup()
+
+contains
+
+
+    subroutine work(num_iter, increase_counters)
+        implicit none
+        
+        ! arguments
+        integer, intent(in) :: num_iter
+        logical, intent(in) :: increase_counters
+        
+        ! local
+        real (kind=wp), save, allocatable :: tmp_field(:, :, :)
+        integer :: iter, i, j, k
+        
+        ! this is only done the first time this subroutine is called (warmup)
+        if (.not. allocated(tmp_field) ) then
+            allocate( tmp_field(nx + 2 * num_halo, ny + 2 * num_halo, nz) )
+            tmp_field = 0.0_wp
+        end if
+        
+        do iter = 1, num_iter
+        
+            call update_halo( in_field(:, :, :), increase_counters=increase_counters )
+            
+            do k = 1, nz
+            do j = 1 + num_halo - 1, ny + num_halo + 1
+            do i = 1 + num_halo - 1, nx + num_halo + 1
+                tmp_field(i, j, k) = -4._wp * in_field(i, j, k)      &
+                    + in_field(i - 1, j, k) + in_field(i + 1, j, k)  &
+                    + in_field(i, j - 1, k) + in_field(i, j + 1, k)
+                if (increase_counters) then
+                    flop_counter = flop_counter + 5
+                    byte_counter = byte_counter + 6 * wp
+                end if
+            end do
+            end do
+            end do
+
+            do k = 1, nz
+            do j = 1 + num_halo, ny + num_halo
+            do i = 1 + num_halo, nx + num_halo
+                out_field(i, j, k) = -4._wp * tmp_field(i, j, k)       &
+                    + tmp_field(i - 1, j, k) + tmp_field(i + 1, j, k)  &
+                    + tmp_field(i, j - 1, k) + tmp_field(i, j + 1, k)
+                if (increase_counters) then
+                    flop_counter = flop_counter + 5
+                    byte_counter = byte_counter + 6 * wp
+                end if
+            end do
+            end do
+            end do
+            
+            if ( iter /= num_iter ) then
+                do k = 1, nz
+                do j = 1 + num_halo, ny + num_halo
+                do i = 1 + num_halo, nx + num_halo
+                    in_field(i, j, k) = out_field(i, j, k)
+                    if (increase_counters) then
+                        byte_counter = byte_counter + 2 * wp
+                    end if
+                end do
+                end do
+                end do
+            end if
+
+        end do
+            
+    end subroutine work
+
+
+    ! implement periodic halo-updates
+    subroutine update_halo( field, increase_counters )
+        implicit none
+            
+        ! argument
+        real (kind=wp), intent(inout) :: field(:, :, :)
+        logical, intent(in) :: increase_counters
+        
+        ! local
+        integer :: i, j, k
+            
+        ! left edge (including corners)
+        field(     1:num_halo,      :, :) =   &
+        field(nx + 1:nx + num_halo, :, :)
+            
+        ! right edge (including corners)
+        field(nx + num_halo + 1:nx + 2 * num_halo, :, :) =   &
+        field(                1:num_halo,          :, :)
+            
+        ! bottom edge (without corners)
+        field(1 + num_halo:nx + num_halo,      1:num_halo,      :) =   &
+        field(1 + num_halo:nx + num_halo, ny + 1:ny + num_halo, :)
+            
+        ! top edge (without corners)
+        field(1 + num_halo:nx + num_halo, ny + num_halo + 1:ny + 2 * num_halo, :) =   &
+        field(1 + num_halo:nx + num_halo,                 1:num_halo,          :)
+        
+        if ( increase_counters ) then
+            byte_counter = byte_counter + 2 * wp * ( 2 * num_halo * nx + 2 * num_halo * ny + 4 * num_halo * num_halo )
+        end if
+
+    end subroutine update_halo
+        
+
+    subroutine init()
+        use mpi, only : MPI_INIT
+        use m_utils, only : error, is_master, timer_init
+        implicit none
+
+        ! local
+        integer :: ierror
+        integer :: i, j, k
+
+        ! initialize MPI environment
+        call MPI_INIT(ierror)
+        call error(ierror /= 0, 'Problem with MPI_INIT', code=ierror)
+
+        call timer_init()
+
+        call read_cmd_line_arguments()
+
+        allocate( in_field(nx + 2 * num_halo, ny + 2 * num_halo, nz) )
+        call random_number( in_field )
+
+        allocate( out_field(nx + 2 * num_halo, ny + 2 * num_halo, nz) )
+        out_field = 0.0_wp
+
+    end subroutine init
+
+
+    subroutine read_cmd_line_arguments()
+        use m_utils, only : error
+        implicit none
+
+        ! local
+        integer iarg, num_arg
+        character(len=256) :: arg, arg_val
+
+        ! setup defaults
+        nx = -1
+        ny = -1
+        nz = -1
+        num_iter = -1
+
+        num_arg = command_argument_count()
+        iarg = 1
+        do while ( iarg <= num_arg )
+            call get_command_argument(iarg, arg)
+            select case (arg)
+            case ("-nx")
+                call error(iarg + 1 > num_arg, "Missing value for -nx argument")
+                call get_command_argument(iarg + 1, arg_val)
+                call error(arg_val(1:1) == "-", "Missing value for -nx argument")
+                read(arg_val, *) nx
+                iarg = iarg + 1
+            case ("-ny")
+                call error(iarg + 1 > num_arg, "Missing value for -ny argument")
+                call get_command_argument(iarg + 1, arg_val)
+                call error(arg_val(1:1) == "-", "Missing value for -ny argument")
+                read(arg_val, *) ny
+                iarg = iarg + 1
+            case ("-nz")
+                call error(iarg + 1 > num_arg, "Missing value for -nz argument")
+                call get_command_argument(iarg + 1, arg_val)
+                call error(arg_val(1:1) == "-", "Missing value for -nz argument")
+                read(arg_val, *) nz
+                iarg = iarg + 1
+            case ("-num_iter")
+                call error(iarg + 1 > num_arg, "Missing value for -num_iter argument")
+                call get_command_argument(iarg + 1, arg_val)
+                call error(arg_val(1:1) == "-", "Missing value for -num_iter argument")
+                read(arg_val, *) num_iter
+                iarg = iarg + 1
+            case default
+                call error(.true., "Unknown command line argument encountered: " // trim(arg))
+            end select
+            iarg = iarg + 1
+        end do
+
+        ! make sure everything is set
+        call error(nx == -1, 'You have to specify nx')
+        call error(ny == -1, 'You have to specify ny')
+        call error(nz == -1, 'You have to specify nz')
+        call error(num_iter == -1, 'You have to specify num_iter')
+
+        ! check consistency of values
+        call error(nx < 0 .or. nx > 1024*1024, "Please provide a reasonable value of nx")
+        call error(ny < 0 .or. ny > 1024*1024, "Please provide a reasonable value of ny")
+        call error(nz < 0 .or. nz > 1024, "Please provide a reasonable value of nz")
+        call error(num_iter < 1 .or. num_iter > 1024*1024, "Please provide a reasonable value of num_iter")
+
+    end subroutine read_cmd_line_arguments
+
+
+    subroutine cleanup()
+        use mpi, only : MPI_FINALIZE, MPI_COMM_WORLD, MPI_DOUBLE_PRECISION, MPI_SUM
+        use m_utils, only : error, timer_print, is_master
+        implicit none
+
+        integer :: ierror
+        real(kind=8) :: global_flop_counter, global_byte_counter
+
+        call MPI_REDUCE(real(flop_counter, 8), global_flop_counter, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+        call MPI_REDUCE(real(byte_counter, 8), global_byte_counter, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+        if ( is_master() ) then
+            write(*, *) 'Total number of GigaFLOP = ', &
+                global_flop_counter / 1024.d0 / 1024.d0 / 1024.d0
+            write(*, *) 'Total number of GByte transferred = ', &
+                global_byte_counter / 1024.d0 / 1024.d0 / 1024.d0
+        end if
+        
+        call timer_print()
+
+        call MPI_FINALIZE(ierror)
+        call error(ierror /= 0, 'Problem with MPI_FINALIZE', code=ierror)
+
+    end subroutine cleanup
+
+end program main
