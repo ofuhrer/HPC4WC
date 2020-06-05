9678ffcc97828fba5ba53789e7552d9352872c7a
ofuhrer <oli@ginko.ch>
2020-06-06 01:17:51 +0200

    update setup, remove xue

--- xue_filter/enter.sh
+++
@@ -1,3 +0,0 @@
-docker stop xue_filter 1>/dev/null 2>/dev/null
-docker rm xue_filter 1>/dev/null 2>/dev/null
-docker run -i -t --rm --mount type=bind,source=`pwd`/work,target=/work --name=xue_filter xue_filter
