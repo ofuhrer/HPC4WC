c1d1b05c0eb6afa07fee447544235b2e0255d4de
Stefano Ubbiali <subbiali@phys.ethz.ch>
2020-06-11 22:45:29 +0200

    Bug fixes.

--- day5/validation.sh
+++ day5/validation.sh
@@ -2,16 +2,18 @@
 
 VERSION=$1
 BACKEND=$2
+PWD=$(pwd)
 
 # generate reference data
 echo "Running stencil2d.py ..."
 cd ../day1 && \
   python stencil2d.py --nx=32 --ny=32 --nz=64 --num_iter=32 && \
-  cd ../day5 || exit
+  cd ../day5 || cd $PWD
 
 # run the programm to validate
 echo "Running stencil2d-gt4py-$VERSION.py ..."
-python stencil2d-gt4py-$VERSION.py --nx=32 --ny=32 --nz=64 --num_iter=32 --backend=$BACKEND
+rm -rf in_field.npy out_field.npy
+python stencil2d-gt4py-$VERSION.py --nx=32 --ny=32 --nz=64 --num_iter=32 --backend=$BACKEND || return
 
 # compare output againts control data
 echo "Running compare_fields.py ..."
