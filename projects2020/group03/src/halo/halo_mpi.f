a2389ceada75b081e36fe7833dfa06f7c47d1964
Michal Sudwoj <msudwoj@student.ethz.ch>
2020-06-25 15:41:44 +0200

    Properly synchronize MPI calls

--- projects2020/group03/src/halo/halo_mpi.f
+++ projects2020/group03/src/halo/halo_mpi.f
@@ -51,6 +51,7 @@ module m_halo_mpi
       allocate(e_recv_buf(num_halo, ny + 2 * num_halo, nz))
       allocate(w_recv_buf(num_halo, ny + 2 * num_halo, nz))
 
+      !$omp critical
       call MPI_Irecv(n_recv_buf, size(n_recv_buf), MPI_FLOAT, p%top(), 0, p%comm(), n_recv_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Irecv', code = ierror)
       call MPI_Irecv(s_recv_buf, size(s_recv_buf), MPI_FLOAT, p%bottom(), 0, p%comm(), s_recv_req, ierror)
@@ -59,6 +60,7 @@ module m_halo_mpi
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Irecv', code = ierror)
       call MPI_Irecv(w_recv_buf, size(w_recv_buf), MPI_FLOAT, p%left(), 0, p%comm(), w_recv_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Irecv', code = ierror)
+      !$omp end critical
 
       ! allocate send buffers
       allocate(n_send_buf(nx, num_halo, nz))
@@ -68,38 +70,46 @@ module m_halo_mpi
 
       ! send north and south
       n_send_buf(:, :, :) = field(1 + num_halo:nx + num_halo, 1 + num_halo:2 * num_halo, :)
+      !$omp critical
       call MPI_Isend(n_send_buf, size(n_send_buf), MPI_FLOAT, p%bottom(), 0, p%comm(), n_send_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Isend', code = ierror)
       s_send_buf(:, :, :) = field(1 + num_halo:nx + num_halo, 1 + ny:ny + num_halo, :)
       call MPI_Isend(s_send_buf, size(s_send_buf), MPI_FLOAT, p%top(), 0, p%comm(), s_send_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Isend', code = ierror)
+      !$omp end critical
 
       ! unpack north and south
+      !$omp critical
       call MPI_Wait(n_recv_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
       call MPI_Wait(s_recv_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
+      !$omp end critical
       field(1 + num_halo:nx + num_halo, 1 + ny + num_halo:ny + 2 * num_halo, :) = n_recv_buf(:, :, :)
       field(1 + num_halo:nx + num_halo, 1:num_halo, :) = s_recv_buf(:, :, :)
 
       ! send east and west
       e_send_buf(:, :, :) = field(1 + num_halo:2 * num_halo, :, :)
+      !$omp critical
       call MPI_Isend(e_send_buf, size(e_send_buf), MPI_FLOAT, p%left(), 0, p%comm(), e_send_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Isend', code = ierror)
       w_send_buf(:, :, :) = field(1 + nx:nx + num_halo, :, :)
       call MPI_Isend(w_send_buf, size(w_send_buf), MPI_FLOAT, p%right(), 0, p%comm(), w_send_req, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Isend', code = ierror)
-
+      !$omp end critical
 
       ! unpack east and west
+      !$omp critical
       call MPI_Wait(e_recv_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', ierror)
       call MPI_Wait(w_recv_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
+      !$omp end critical
       field(1 + nx + num_halo:nx + 2 * num_halo, :, :) = e_recv_buf(:, :, :)
       field(1:num_halo, :, :) = w_recv_buf(:, :, :)
 
       ! await sends
+      !$omp critical
       call MPI_Wait(n_send_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
       call MPI_Wait(s_send_req, MPI_STATUS_IGNORE, ierror)
@@ -108,6 +118,7 @@ module m_halo_mpi
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
       call MPI_Wait(w_send_req, MPI_STATUS_IGNORE, ierror)
       call error(ierror /= MPI_SUCCESS, 'Problem with MPI_Wait', code = ierror)
+      !$omp end critical
     end subroutine
 end module
 
