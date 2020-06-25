f3b8cba1c1a27b62897829dc64ce86f8d0a5a23f
Michal Sudwoj <msudwoj@student.ethz.ch>
2020-06-25 17:59:08 +0200

    Corrected CMake version

--- projects2020/group03/scripts/daint.sh
+++ projects2020/group03/scripts/daint.sh
@@ -4,4 +4,4 @@ module load daint-gpu cudatoolkit
 SSL_SPACK_ROOT=/apps/daint/SSL/software/spack-current
 export PATH=${PATH}:${SSL_SPACK_ROOT}/bin
 source ${SSL_SPACK_ROOT}/share/spack/setup-env.sh
-spack load cmake@3.17.2
+spack load cmake@3.17.1
