982b64ccf2a86320cbfcbaa282ec0bad3da8d917
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2020-07-03 10:27:07 +0200

    additionally assert whethere only non-nan values are the same

--- projects2020/group09/unittest_simple.py
+++ projects2020/group09/unittest_simple.py
@@ -10,6 +10,12 @@ import numpy as np
 def test_simple(mod, obs):
 
     assert mod.shape == obs.shape, 'shapes are not the same'
+    
+    assert np.isclose(np.nan_to_num(mod)[:,2:-3,2:-3,2:-3], np.nan_to_num(obs)[:,2:-3,2:-3,2:-3], equal_nan=True).all(), 'non-nan values outside boundaries are not the same within tolerance'
+
+    assert np.isclose(np.nan_to_num(mod), np.nan_to_num(obs), equal_nan=True).all(), 'non-nan values are not the same within tolerance'
+
+    assert np.isclose(mod[:,2:-3,2:-3,2:-3], obs[:,2:-3,2:-3,2:-3], equal_nan=True).all(), 'values outside boundaries are not the same within tolerance'
 
     assert np.isclose(mod, obs, equal_nan=True).all(), 'values are not the same within tolerance'
 
