7a5a4f16d64a0d4e8309519ec01a12bd7fbbc43b
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2020-07-22 16:47:08 +0200

    update cython block version

--- projects2020/group09/baseline_oli.py
+++ projects2020/group09/baseline_oli.py
@@ -26,7 +26,7 @@ print(data.shape)
 # numpy 
 tic = datetime.now()
 footprint = np.ones((1,5,5,5))
-tmp = generic_filter(data, np.nanmean, footprint=footprint, mode='nearest')
+#tmp = generic_filter(data, np.nanmean, footprint=footprint, mode='nearest')
 toc = datetime.now()
 print(f'numpy {toc-tic}')
 
@@ -108,7 +108,81 @@ toc = datetime.now()
 vali = np.isclose(result[:,2:-2,2:-2,2:-2], tmp[:,2:-2,2:-2,2:-2], equal_nan=True).all()
 print(f'cython {toc-tic}, validated: {vali}')
 
+# cython stencil
+from cython_loop import stencil_loop_blocking as stencil_loop
+mask = (~ np.isnan(data.values) *1.)
+mask = mask.astype(np.float32)
+tic = datetime.now()
+result = np.empty(data.shape, dtype=np.float32)
+weights = np.empty(data.shape, dtype=np.float32)
+result[0,:,:,:] = stencil_loop(datanum[0,:,:,:])
+result[1,:,:,:] = stencil_loop(datanum[1,:,:,:])
+result[2,:,:,:] = stencil_loop(datanum[2,:,:,:])
+weights[0,:,:,:] = stencil_loop(mask[0,:,:,:])
+weights[1,:,:,:] = stencil_loop(mask[1,:,:,:])
+weights[2,:,:,:] = stencil_loop(mask[2,:,:,:])
+result = result / weights
+result = np.where(weights == 0, np.nan, result)
+toc = datetime.now()
+vali = np.isclose(result[:,2:-2,2:-2,2:-2], tmp[:,2:-2,2:-2,2:-2], equal_nan=True).all()
+print(f'cython block {toc-tic}, validated: {vali}')
 
+# stencil debugging area
+nx = 11 # total size
+ny = 11
+nz = 11
+blockx = 5 # block size
+blocky = 5
+blockz = 5
+blocki = nx//blockx+1 # number of blocks (last one smaller)
+blockj = ny//blocky+1
+blockk = nz//blockz+1
+
+blocksizes_x = [blockx] * (nx//blockx) + [nx%blockx]
+blocksizes_y = [blocky] * (ny//blocky) + [ny%blocky]
+blocksizes_z = [blockz] * (nz//blockz) + [nz%blockz]
+print(blocksizes_x)
+print(blocksizes_y)
+print(blocksizes_z)
+
+A = np.zeros((nx,ny,nz))
+for iblock, iblocklen in enumerate(blocksizes_x):
+    for jblock, jblocklen in enumerate(blocksizes_y):
+        for kblock, kblocklen in enumerate(blocksizes_z):
+            for i_local in range(iblocklen):
+                for j_local in range(jblocklen):
+                    for k_local in range(kblocklen):
+                        i = iblock*blockx + i_local
+                        j = jblock*blocky + j_local
+                        k = kblock*blockz + k_local
+                        A[i,j,k] = 1
+
+print(A.size, A.sum())
+A = np.zeros((nx,ny,nz))
+for iblock in range(blocki):
+    if iblock == blocki-1: # we are in the last block
+        iblocklen = nx%blockx
+    else: # we are not in the last block
+        iblocklen = blockx
+    for jblock in range(blockj):
+        if jblock == blockj-1: # we are in the last block
+            jblocklen = ny%blocky
+        else: # we are not in the last block
+            jblocklen = blocky
+        #print(jblock, blockj, jblocklen)
+        for kblock in range(blockk):
+            if kblock == blockk-1: # we are in the last block
+                kblocklen = nz%blockz
+            else: # we are not in the last block
+                kblocklen = blockz
+            for i_local in range(iblocklen):
+                for j_local in range(jblocklen):
+                    for k_local in range(kblocklen):
+                        i = iblock*blockx + i_local
+                        j = jblock*blocky + j_local
+                        k = kblock*blockz + k_local
+                        A[i,j,k] = 1
+print(A.size, A.sum())
 #data = data.fillna(tmp)
 # save result as "ground truth" for testing other approaches
 #data.to_netcdf('baseline_result.nc')
