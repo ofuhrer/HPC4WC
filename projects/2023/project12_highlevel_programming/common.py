79b6dc9207eea916d8be9e2de1c641a3b3f2efd4
Iyán Méndez Veiga <iyan.mendezveiga@hslu.ch>
2023-08-19 11:39:14 +0200

    Add num_halo kwarg to initialize_fields()

--- projects/2023/project12_highlevel_programming/common.py
+++ projects/2023/project12_highlevel_programming/common.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def initialize_fields(NX, NY, NZ, mode="random", order="C", dtype=np.float64):
+def initialize_fields(NX, NY, NZ, mode="random", num_halo=0, order="C", dtype=np.float64):
     """
     This function initializes the 3D fields with some patterns to help validating
     the stencil update functions.
@@ -14,14 +14,15 @@ def initialize_fields(NX, NY, NZ, mode="random", order="C", dtype=np.float64):
     in_field = np.zeros([NZ, NY, NX], order=order, dtype=dtype)
     
     if mode == "random":
-        rng.random(size=[NZ, NY, NX], out=in_field, dtype=dtype)
+        tmp = rng.random(size=[NZ, NY - 2 * num_halo, NX - 2 * num_halo], dtype=dtype)
         # Uniformly distributed in [-1, 1)
-        in_field = 2 * in_field - 1  
+        in_field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
     elif mode == "horizontal-bars":
-        in_field[:,: NY//2 : 2, :] = 1
+        in_field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
     elif mode == "vertical-bars":
-        in_field[:, :,: NX//2 : 2] = 1
+        in_field[:, num_halo : NY - num_halo, num_halo : NX//2 - num_halo : 2] = 1
     elif mode == "square":
+        # num_halo is ignored in this mode
         in_field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
     else:
         raise ValueError("Wrong mode")
