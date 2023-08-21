b098817c7d31d90a88a43ab8b4a125ab85279df3
dymastery <dymastery@outlook.com>
2023-08-21 17:00:10 +0200

    Write GT4Py copy and 1D stencil. With GT4Py 1.0.1, numpy 1.21.6, only numpy backend successful. Rewrite initiate function in common.py.

--- projects/2023/project12_highlevel_programming/common.py
+++ projects/2023/project12_highlevel_programming/common.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import gt4py as gt
+import gt4py as gt
 
-def initialize_fields(NX, NY, NZ, mode="random", num_halo=0, order="C", dtype=np.float64):
+def initialize_fields(NX, NY, NZ, mode="random", num_halo=0, order="C", dtype=np.float64, ifirst=False):
     """
     This function initializes the 3D fields with some patterns to help validating
     the stencil update functions.
@@ -12,45 +12,72 @@ def initialize_fields(NX, NY, NZ, mode="random", num_halo=0, order="C", dtype=np
     
     # Initialize 3D fields
     rng = np.random.default_rng()
-
-    in_field = np.zeros([NZ, NY, NX], order=order, dtype=dtype)
     
-    if mode == "random":
-        tmp = rng.random(size=[NZ, NY - 2 * num_halo, NX - 2 * num_halo], dtype=dtype)
-        # Uniformly distributed in [-1, 1)
-        in_field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
-    elif mode == "horizontal-bars":
-        in_field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
-    elif mode == "vertical-bars":
-        in_field[:, num_halo : NY - num_halo, num_halo : NX//2 - num_halo : 2] = 1
-    elif mode == "square":
-        # num_halo is ignored in this mode
-        in_field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
-    else:
-        raise ValueError("Wrong mode")
+    # Initiate field under sequence [k,j,i]
+    if ifirst==False:
+        in_field = np.zeros([NZ, NY, NX], order=order, dtype=dtype)
 
+        if mode == "random":
+            tmp = rng.random(size=[NZ, NY - 2 * num_halo, NX - 2 * num_halo], dtype=dtype)
+            # Uniformly distributed in [-1, 1)
+            in_field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
+        elif mode == "horizontal-bars":
+            in_field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
+        elif mode == "vertical-bars":
+            in_field[:, num_halo : NY - num_halo, num_halo : NX//2 - num_halo : 2] = 1
+        elif mode == "square":
+            # num_halo is ignored in this mode
+            in_field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
+        else:
+            raise ValueError("Wrong mode")
+    
+    # Initiate field under sequence [i,j,k]
+    else:
+        in_field = np.zeros([NX, NY, NZ], order=order, dtype=dtype)
+
+        if mode == "random":
+            tmp = rng.random(size=[NX - 2 * num_halo, NY - 2 * num_halo, NZ], dtype=dtype)
+            # Uniformly distributed in [-1, 1)
+            in_field[num_halo : NX - num_halo, num_halo : NY - num_halo, :] = 2 * tmp - 1  
+        elif mode == "horizontal-bars":
+            in_field[num_halo : NX//2 - num_halo : 2, num_halo : NY - num_halo, :] = 1
+        elif mode == "vertical-bars":
+            in_field[num_halo : NX - num_halo, num_halo : NY//2 - num_halo : 2, :] = 1
+        elif mode == "square":
+            # num_halo is ignored in this mode
+            in_field[NX//2 - NX//4 : NX//2 + NX//4, NY//2 - NY//4 : NY//2 + NY//4, :] = 1
+        else:
+            raise ValueError("Wrong mode")
+            
     out_field = np.copy(in_field)
     
     return in_field, out_field
 
-def plot_field(field, k=0):
+def plot_field(field, k=0, ifirst=False):
     field = np.array(field)
     plt.figure(figsize=(7, 5), dpi=100)
-    plt.imshow(field[k, :, :], origin='lower', vmin=-1, vmax=1);
-    plt.colorbar();
+    
+    if ifirst == False:
+        plt.imshow(field[k, :, :], origin='lower', vmin=-1, vmax=1);
+        plt.colorbar();
+    else:
+        plt.imshow(field[:, :, k], origin='lower', vmin=-1, vmax=1);
+        plt.colorbar();        
 
 def array_to_gt_storage(in_field, out_field, dtype=np.float64, backend="numpy", index=(0, 0, 0)):
-    # I still need time to figure out the usage of dimensions
-    # attributes, so now a transpose is used
-    in_field = gt.storage.from_array(
-        in_field.swapaxes(0,2),
+    in_field_gt = gt.storage.from_array(
+        in_field,
         dtype=dtype,
         backend=backend,
         aligned_index=index
     )
-    out_field = gt.storage.from_array(
-        out_field.swapaxes(0,2),
+    
+    out_field_gt = gt.storage.from_array(
+        out_field,
         dtype=dtype,
         backend=backend,
         aligned_index=index
     )
+    
+    return in_field_gt, out_field_gt
+
