215400ae3e0ef33e19f1d9fdf68f7de843b7bcf9
Iyán Méndez Veiga <iyan.mendezveiga@hslu.ch>
2023-08-21 13:38:03 +0200

    Fix setup

--- projects/2023/project12_highlevel_programming/setup.sh
+++ projects/2023/project12_highlevel_programming/setup.sh
@@ -17,14 +17,15 @@ module load daint-gpu
 module load cray-python
 module load jupyter-utils
 
-echo "Creating virtual HPC4WC_venv Python virtual environment"
+echo "Creating virtual HPC4WC_project12_venv Python virtual environment"
 python -m venv HPC4WC_project12_venv
 source HPC4WC_project12_venv/bin/activate
+pip install --upgrade pip
 pip install setuptools wheel
 MPICC=CC pip install -r ~/HPC4WC_project12/projects/2023/project12_highlevel_programming/requirements.txt
 
 echo "Creating HPC4WC_project12_kernel kernel for Jupyter"
-cp ~/HPC4WC_project12_venv/setup/etc/.jupyterhub.env ~/
+cp ~/HPC4WC_project12/setup/etc/.jupyterhub.env ~/
 kernel-create -n HPC4WC_project12_kernel
 sed -i "s/if \[ \"\$SOURCE_JUPYTERHUBENV\" == true \]\; then//" ~/.local/share/jupyter/kernels/HPC4WC_project12_kernel/launcher
 sed -i "s/fi//" ~/.local/share/jupyter/kernels/HPC4WC_project12_kernel/launcher
