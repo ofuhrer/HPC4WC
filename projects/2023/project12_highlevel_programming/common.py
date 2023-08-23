e4f5e558f304abb49db7bf86396011b3cfe68456
Iyán Méndez Veiga <me@iyanmv.com>
2023-08-23 20:57:27 +0200

    Simplify initialize_field() and array_to_gt_storage()
    We were returning two arrays for no good reason. I realize that this
    makes it more difficul to check the correctness of some cases. If we
    need to copy the array, let's do that in the notebook directly.

--- projects/2023/project12_highlevel_programming/common.py
+++ projects/2023/project12_highlevel_programming/common.py
@@ -7,29 +7,29 @@ import gt4py as gt
 rng = np.random.default_rng(1337)
 
 
-def initialize_fields(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, array_order="C", dtype=np.float64):
+def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, array_order="C", dtype=np.float64):
     """
     This function initializes the 3D fields with some patterns to help validating
     the stencil update functions.
     """
 
-    assert num_halo < NX // 2 and num_halo < NY // 2
+    assert 0 <= num_halo < NX // 2 and 0 <= num_halo < NY // 2
     
     # We do all the initialization assuming the dim_order="ZYX"
-    in_field = np.zeros([NZ, NY, NX], dtype=dtype)
+    field = np.zeros([NZ, NY, NX], dtype=dtype)
 
     assert isinstance(mode, str)
     if mode == "random":
         tmp = rng.random(size=[NZ, NY - 2 * num_halo, NX - 2 * num_halo], dtype=dtype)
         # Uniformly distributed in [-1, 1)
-        in_field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
+        field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
     elif mode == "horizontal-bars":
-        in_field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
+        field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
     elif mode == "vertical-bars":
-        in_field[:, num_halo : NY - num_halo, num_halo : NX//2 - num_halo : 2] = 1
+        field[:, num_halo : NY - num_halo, num_halo : NX//2 - num_halo : 2] = 1
     elif mode == "square":
         # num_halo is ignored in this mode
-        in_field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
+        field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
     else:
         raise ValueError("Wrong mode")
     
@@ -38,26 +38,25 @@ def initialize_fields(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, ar
     if dim_order == "ZYX":
         pass
     elif dim_order == "XZY":
-        in_field = np.transpose(in_field, axes=[2, 0, 1])
+        field = np.transpose(field, axes=[2, 0, 1])
     elif dim_order == "YXZ":
-        in_field = np.transpose(in_field, axes=[1, 2, 0])
+        field = np.transpose(field, axes=[1, 2, 0])
     elif dim_order == "XYZ":
-        in_field = np.transpose(in_field, axes=[2, 1, 0])
+        field = np.transpose(field, axes=[2, 1, 0])
     elif dim_order == "ZXY":
-        in_field = np.transpose(in_field, axes=[0, 2, 1])
+        field = np.transpose(field, axes=[0, 2, 1])
     elif dim_order == "YZX":
-        in_field = np.transpose(in_field, axes=[1, 0, 2])
+        field = np.transpose(field, axes=[1, 0, 2])
     else:
         raise ValueError("Wrong dim order")
             
-    out_field = np.copy(in_field)
     
     # Enforce returning arrays using the right C or Fortran order
     assert isinstance(array_order, str)
     if array_order == "C":
-        return np.ascontiguousarray(in_field), np.ascontiguousarray(out_field)
+        return np.ascontiguousarray(field)
     elif array_order == "F":
-        return np.asfortranarray(in_field), np.asfortranarray(out_field)
+        return np.asfortranarray(field)
     else:
         raise ValueError("Wrong array order")
     
@@ -88,19 +87,10 @@ def plot_field(in_field, dim_order="ZYX", k=0):
     plt.colorbar();
              
 
-def array_to_gt_storage(in_field, out_field, dtype=np.float64, backend="numpy", index=(0, 0, 0)):
-    in_field_gt = gt.storage.from_array(
-        in_field,
+def array_to_gt_storage(field, dtype=np.float64, backend="numpy", index=(0, 0, 0)):
+    return gt.storage.from_array(
+        field,
         dtype=dtype,
         backend=backend,
         aligned_index=index
     )
-    
-    out_field_gt = gt.storage.from_array(
-        out_field,
-        dtype=dtype,
-        backend=backend,
-        aligned_index=index
-    )
-    
-    return in_field_gt, out_field_gt
