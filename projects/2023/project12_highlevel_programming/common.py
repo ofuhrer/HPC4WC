61bba0ce3e4b76cf814807b3db54069d673dbb4b
dymastery <dymastery@outlook.com>
2023-08-24 11:41:44 +0200

    Delete array_to_gt_storage in common.py as no longer needed. Add some parts for 2D stencil and texts.

--- projects/2023/project12_highlevel_programming/common.py
+++ projects/2023/project12_highlevel_programming/common.py
@@ -88,15 +88,6 @@ def plot_field(in_field, dim_order="ZYX", k=0):
     
     plt.imshow(field[k, :, :], origin='lower', vmin=-1, vmax=1);    
     plt.colorbar();
-             
-
-def array_to_gt_storage(field, dtype=np.float64, backend="numpy", index=(0, 0, 0)):
-    return gt.storage.from_array(
-        field,
-        dtype=dtype,
-        backend=backend,
-        aligned_index=index
-    )
 
 
 def save_result(result, test_name, file="results.csv", overwrite=False, header=False):
