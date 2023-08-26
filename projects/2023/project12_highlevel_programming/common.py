2f99511327d98863000b1bba31a7bbd2fbf64e73
Iyán Méndez Veiga <me@iyanmv.com>
2023-08-26 11:00:21 +0200

    Add read_results() function

--- projects/2023/project12_highlevel_programming/common.py
+++ projects/2023/project12_highlevel_programming/common.py
@@ -1,5 +1,6 @@
 import socket
 import numpy as np
+import pandas as pd
 import IPython
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, arr
     """
 
     assert 0 <= num_halo < NX // 2 and 0 <= num_halo < NY // 2
-    
+
     # We do all the initialization assuming the dim_order="ZYX"
     field = np.zeros([NZ, NY, NX], dtype=dtype)
 
@@ -26,7 +27,7 @@ def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, arr
         # for that reason we create a copy with the desired dtype using astype()
         tmp = rng.random(size=[NZ, NY - 2 * num_halo, NX - 2 * num_halo]).astype(dtype)
         # Uniformly distributed in [-1, 1)
-        field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1  
+        field[:, num_halo : NY - num_halo, num_halo : NX - num_halo] = 2 * tmp - 1
     elif mode == "horizontal-bars":
         field[:, num_halo : NY//2 - num_halo : 2, num_halo : NX - num_halo] = 1
     elif mode == "vertical-bars":
@@ -36,7 +37,7 @@ def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, arr
         field[:, NY//2 - NY//4 : NY//2 + NY//4, NX//2 - NX//4 : NX//2 + NX//4] = 1
     else:
         raise ValueError("Wrong mode")
-    
+
     # Rearrange dimensions to dim_order
     assert isinstance(dim_order, str)
     if dim_order == "ZYX":
@@ -53,8 +54,8 @@ def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, arr
         field = np.transpose(field, axes=[1, 0, 2])
     else:
         raise ValueError("Wrong dim order")
-            
-    
+
+
     # Enforce returning arrays using the right C or Fortran order
     assert isinstance(array_order, str)
     if array_order == "C":
@@ -63,13 +64,13 @@ def initialize_field(NX, NY, NZ, dim_order="ZYX", mode="random", num_halo=0, arr
         return np.asfortranarray(field)
     else:
         raise ValueError("Wrong array order")
-    
-    
+
+
 def plot_field(in_field, dim_order="ZYX", k=0):
     field = np.array(in_field)
-    
+
     plt.figure(figsize=(7, 5), dpi=100)
-    
+
     # Rearrange dimensions to the desire
     assert isinstance(dim_order, str)
     if dim_order == "ZYX":
@@ -86,10 +87,10 @@ def plot_field(in_field, dim_order="ZYX", k=0):
         field = np.transpose(field, axes=[1, 0, 2])
     else:
         raise ValueError("Wrong dim order")
-    
-    plt.imshow(field[k, :, :], origin='lower', vmin=-1, vmax=1);    
+
+    plt.imshow(field[k, :, :], origin='lower', vmin=-1, vmax=1);
     plt.colorbar();
-             
+
 
 def array_to_gt_storage(field, dtype=np.float64, backend="numpy", index=(0, 0, 0)):
     import gt4py as gt
@@ -104,15 +105,50 @@ def array_to_gt_storage(field, dtype=np.float64, backend="numpy", index=(0, 0, 0
 def save_result(result, test_name=None, file="results.csv", overwrite=False, header=False):
     if overwrite:
         open(file, "w").close()
-    
+
     with open(file, "a") as f:
         if header:
             print("timestamp,hostname,test,timeit_avg,timeit_std", file=f)
-        
+
         if result is not None:
             assert isinstance(result, IPython.core.magics.execution.TimeitResult)
             assert isinstance(test_name, str)
             print(f"{datetime.utcnow()},{socket.gethostname()},{test_name},{result.average:.2e},{result.stdev:.2e}", file=f)
+
+
+def read_results(test_name, file="results.csv", hostname=None, std=False, as_array=True):
+    assert isinstance(test_name, list) or isinstance(test_name, str)
+
+    # dataframe.isin() expects a list
+    if isinstance(test_name, str):
+        test_name = [test_name]
+
+    results = pd.read_csv(file)
+
+    if hostname is None:
+        selection = results["test"].isin(test_name)
+    else:
+        assert isinstance(hostname, list) or isinstance(hostname, str)
+        if isinstance(hostname, str):
+            hostname = [hostname]
+        selection = (results["test"].isin(test_name)) & (results["hostname"].isin(hostname))
+
+    output = results.loc[selection]
+
+    if as_array:
+        if std:
+            output = output.loc[:, ["timeit_avg", "timeit_std"]].to_numpy()
+        else:
+            output = output.loc[:, ["timeit_avg"]].to_numpy()
+
+        # We don't want size 0 or 1 arrays. If that's the case return the None or the number
+        if output.size == 0:
+            return None
+        elif output.size == 1:
+            return output.item()
+        return output
+
+    return output
 
 
 def compare_results(a, b, mode="faster"):
