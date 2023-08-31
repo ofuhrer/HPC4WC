c9e993be1bc56ba2d287c4f162c2f1bcce0f02c4
thisfro <jannis@thisfro.ch>
2023-08-31 14:45:09 +0200

    Add visual validation and handle cupy

--- projects/2023/project07_dsl_in_python/dsl/driver/driver.py
+++ projects/2023/project07_dsl_in_python/dsl/driver/driver.py
@@ -1,16 +1,23 @@
 import time
 import numpy as np
-# import cupy as cp
-from dsl.generated.main import generated_function
+import matplotlib.pyplot as plt
 
+try:
+    import cupy as cp
+    CUDA = True
+except ImportError:
+    CUDA = False
+    
+
+from dsl.generated.main import generated_function
 from dsl.baseline.baseline_stencil import baseline_stencil
 
 
 def main(kinds=None):
     # Input fields
-    nx = 10
-    ny = 10
-    nz = 10
+    nx = 20
+    ny = 20
+    nz = 20
     num_iter = 10
     num_halo = 2
     alpha = 1.0 / 32.0
@@ -32,9 +39,13 @@ def main(kinds=None):
     out_fields = {}
 
     for kind in kinds:
-        start_times[kind] = time.time()
-        out_fields[kind] = run_function(kind, in_field, out_field, num_halo, nx, ny, nz, num_iter, tmp_field, alpha)
-        exec_times[kind] = time.time() - start_times[kind]
+        start_times[kind] = []
+        exec_times[kind] = []
+        
+        for i in range(10):
+            start_times[kind].append(time.time())
+            out_fields[kind] = run_function(kind, in_field, out_field, num_halo, nx, ny, nz, num_iter, tmp_field, alpha)
+            exec_times[kind].append(time.time() - start_times[kind][i])
 
     # Output validaiton
     validation(out_fields)
@@ -50,7 +61,13 @@ def run_function(kind, in_field, out_field, num_halo, nx, ny, nz, num_iter, tmp_
         return baseline_stencil(in_field, out_field, num_halo, nx, ny, nz, num_iter, tmp_field, alpha)
     if kind == "generated":
         return generated_function(in_field, out_field, num_halo, nx, ny, nz, num_iter, tmp_field, alpha)
-
+    if kind == "cuda":
+        in_field_cp = cp.asarray(in_field)
+        out_field_cp = cp.asarray(out_field)
+        tmp_field_cp = cp.asarray(tmp_field)
+        
+        out_field_cp = generated_function(in_field_cp, out_field_cp, num_halo, nx, ny, nz, num_iter, tmp_field_cp, alpha)
+        return out_field_cp.get()
 
 def validation(out_fields):
     max_key_length = max(len(key) for key in out_fields.keys())
@@ -60,8 +77,14 @@ def validation(out_fields):
     print("================")
 
     for key, value in out_fields.items():
+        plt.imshow(out_fields[key][out_fields[key].shape[0] // 2, :, :], origin="lower")
+        plt.colorbar()
+        plt.tight_layout()
+        plt.savefig(f"plot_{key}.png")
+        plt.close()
+        
         if key != "base":
-            if (out_fields["base"] == out_fields[key]).all():
+            if np.allclose(out_fields["base"], out_fields[key], rtol=1e-5, atol=1e-8, equal_nan=True):
                 output = "{:<{width}}: passed".format(key, width=max_key_length)
             else:
                 output = "{:<{width}}: failed".format(key, width=max_key_length)
@@ -75,10 +98,12 @@ def diagnostics(times):
     print("Execution times:")
     print("================")
     for key, value in times.items():
-        output = "{:<{width}}: {:>}ms".format(key, round(value * 1000, 2), width=max_key_length)
+        output = "{:<{width}}: {:>}ms, (sd: {:>}ms)".format(key, round(np.mean(value) * 1000, 2), round(np.std(value) * 1000, 2), width=max_key_length)
         print(output)
 
 
 if __name__ == "__main__":
     kinds = ["generated", "base"]
+    if CUDA:
+        kinds.append("cuda")
     main(kinds=kinds)
