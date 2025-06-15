9efdb466de0f285157b8115c5019a96efb5f1ef4
Oliver Fuhrer <oli@ginko.ch>
2025-06-15 17:25:58 +0200

    Update setup

--- setup/HPC4WC_setup.sh
+++ setup/HPC4WC_setup.sh
@@ -11,10 +11,6 @@ if [ -d HPC4WC_venv ] ; then
     exit 1
 fi
 
-module load daint-gpu
-module load cray-python
-module load jupyter-utils
-
 echo "Making a backup copy of HPC4WC to HPC4WC_orig"
 cp -r HPC4WC HPC4WC_orig
 
@@ -23,14 +19,10 @@ python -m venv HPC4WC_venv
 source HPC4WC_venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install setuptools wheel
-MPICC=CC python -m pip install -r ~/HPC4WC/setup/etc/requirements.txt
+python -m pip install -r ~/HPC4WC/setup/etc/requirements.txt
 
 echo "Creating HPC4WC_kernel kernel for Jupyter"
-cp ~/HPC4WC/setup/etc/.jupyterhub.env ~/
-kernel-create -n HPC4WC_kernel
-sed -i "s/if \[ \"\$SOURCE_JUPYTERHUBENV\" == true \]\; then//" ~/.local/share/jupyter/kernels/HPC4WC_kernel/launcher
-sed -i "s/fi//" ~/.local/share/jupyter/kernels/HPC4WC_kernel/launcher
-sed -i "s/export PYTHONPATH=''//" ~/.local/share/jupyter/kernels/HPC4WC_kernel/launcher
+python -m ipykernel install --user --name="HPC4WC_kernel" --display-name="HPC4WC_kernel"
 
 echo "Sucessfully finished. You must restart your JupyterHub now!"
 
